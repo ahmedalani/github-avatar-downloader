f32a8bf1eb63a9ce809e7bf77725fa470fd31980
Ahmed Alani <s3leha@gmail.com>
2017-09-12 01:42:36 +0000

    the test request function to download test picture worked

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,6 @@
 var request = require('request');
+var fs = require('fs');
+
 
 // console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -29,6 +31,20 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+function downloadImageByURL(url, filePath) {
+
+
+}
+
+request.get('https://avatars2.githubusercontent.com/u/2741?v=3&s=466')
+   .on('error', function (err) {
+     throw err;
+   })
+   .on('response', function (response) {
+     console.log('Response Status Code: ', response.statusCode);
+   })
+   .pipe(fs.createWriteStream('./kvirani.jpg'));
 
 
 
