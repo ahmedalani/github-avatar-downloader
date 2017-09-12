4d044047ee7578411b7b6e33b349dfc54242d70a
Ahmed Alani <s3leha@gmail.com>
2017-09-12 02:47:35 +0000

    looped through contributors object and downloaded the avatars

--- download_avatars.js
+++ download_avatars.js
@@ -9,7 +9,6 @@ var GITHUB_TOKEN = "22967f63f7c9cdde33628425b608998b83dc17ad";
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
   const option = {
     url: 'https://api.github.com/repos/jquery/jquery/contributors',
       qs: {
@@ -27,24 +26,43 @@ function getRepoContributors(repoOwner, repoName, cb) {
 }
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors:", err);
+//   console.log("Result:", result);
+// });
+
+
+getRepoContributors("jquery", "jquery", function(err, contributorsData) {
+
+  for (var key in contributorsData) {
+    const user = contributorsData[key];
+
+    if (user.login) {
+      // .pipe(fs.createWriteStream('./avatars/contributorsData.key' + '.png');
+      downloadImageByURL(user["avatar_url"], './avatars/'+user.login + '.png');
+    }
+  }
+
+
+
 });
 
 function downloadImageByURL(url, filePath) {
+  console.log('Downloading:', url, '\tto:', filePath);
+  request.get(url)
+    .on('error', function (err) {
+      throw err;
+    })
+    .on('response', function (response) {
+      console.log('Response Status Code: ', response.statusCode);
+    })
+    .pipe(fs.createWriteStream(filePath));
+};
+
+// downloadImageByURL('https://avatars2.githubusercontent.com/u/2741?v=3&s=466', './kvirani.jpg');
 
 
-}
 
-request.get('https://avatars2.githubusercontent.com/u/2741?v=3&s=466')
-   .on('error', function (err) {
-     throw err;
-   })
-   .on('response', function (response) {
-     console.log('Response Status Code: ', response.statusCode);
-   })
-   .pipe(fs.createWriteStream('./kvirani.jpg'));
 
 
 
