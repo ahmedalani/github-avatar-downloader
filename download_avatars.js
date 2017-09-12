fe031edaf1b684e0b1e0a8d4894f9bd79321b697
Ahmed Alani <s3leha@gmail.com>
2017-09-12 01:22:09 +0000

    reorganized code and fixed indentations

--- download_avatars.js
+++ download_avatars.js
@@ -8,32 +8,27 @@ var GITHUB_TOKEN = "22967f63f7c9cdde33628425b608998b83dc17ad";
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   console.log(requestURL);
+  const option = {
+    url: 'https://api.github.com/repos/jquery/jquery/contributors',
+      qs: {
+        access_token: GITHUB_TOKEN
+      },
+      headers: {
+        'User-Agent': 'anything'
+      }
+    };
+  request(option, function(err, response, body) {
+    // console.log('body:', JSON.parse(body));
+    cb(err, JSON.parse(body))
+  });
+
 }
 
-var request = require('request');
-
-request('https://sytantris.github.io/http-examples/', function(err, response, body) {
-  if (err) throw err;
-  console.log('Response Status Code:', response.statusCode);
-});
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
 
-const option = {
-url: 'https://api.github.com/repos/jquery/jquery/contributors',
-    qs: {
-
-      access_token: GITHUB_TOKEN
-    },
-    headers: {
-      'User-Agent': 'anything'
-    }
-  };
 
 
-request(option, function(err, response, body) {
-  console.log('body:', JSON.parse(body));
-});
